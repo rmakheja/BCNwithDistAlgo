481116736df4cd8024b3093c2dd1de1134442c38
sansanketdg <sanketdige268@gmail.com>
2017-10-23 16:03:00 -0400

    added all commands in run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
+python3 -m da -H 10.142.0.2 -n OlympusNode -f --logfilename logfile.txt --message-buffer-size=$((64*1024)) -D Olympus.da
 python3 -m da -H 10.142.0.3 -n ReplicaNode -f --logfilename logfile.txt --message-buffer-size=$((64*1024)) -D Replica.da
+python3 -m da -H 10.142.0.4 -n ReplicaNode -f --logfilename logfile.txt --message-buffer-size=$((64*1024)) -D Client.da
+#python3 -m da -H 10.142.0.5 -n MasterNode -f --logfilename logfile.txt --message-buffer-size=$((64*1024)) -D __init__.da system.conf
